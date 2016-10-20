9516e79744df838b02e6c266cfedf4be8bdf7136
DOOMer <doomer3d@gmail.com>
2016-10-20 12:05:24 +0300

    Add defines for "display secondss" and "display date" cvars

--- src/globals.h
+++ src/globals.h
@@ -5,6 +5,8 @@
 #define QS_BLOCK_MAIN "main"
 #define QS_BLOCK_WINDOW "window"
 #define QS_ITEM_GEOMETRY "geometry"
+#define QS_ITEM_DISPLAY_SECS "displaySeconds"
+#define QS_ITEM_DISPLAY_DATE "displayDate"
 
 const int DEF_BKG_R = 0;
 const int DEF_BKG_G = 0;
